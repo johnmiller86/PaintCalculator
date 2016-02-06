fe2562e786317fec3a9434b17f34fa09f41ff5da
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-06 17:10:45 -0500

    Initial commit

---
+++ app/src/main/java/com/twh5257_jdm5908_bw/ist402/paintcalculator/HelpActivity.java
@@ -0,0 +1,13 @@
+package com.twh5257_jdm5908_bw.ist402.paintcalculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_help);
+    }
+}
---
+++ app/src/main/java/com/twh5257_jdm5908_bw/ist402/paintcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.twh5257_jdm5908_bw.ist402.paintcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
